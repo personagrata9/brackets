e2ba7fd265bfd79f28c804b3562d5455293c013e
personagrata9 <ira.bakhur@icloud.com>
2021-03-16 02:53:32 +0300

    feat: add partial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let match;
+  let brackets = bracketsConfig.toString().split(',');
+  let brackets1 = brackets.filter((element, index)=> index % 2 === 0);
+  let brackets2 = brackets.filter((element, index)=> index % 2 !== 0);
+
+  for (let i = 0; i < str.length; i++) {
+    if (brackets2.indexOf(str[i]) > -1) {
+      match = brackets1[brackets2.indexOf(str[i])]
+if (stack.length == 0 || (stack.pop() != match)) {
+return false;
+}
+} else {
+stack.push(str[i]);
+}
+}
+return (stack.length == 0);
 }
